6e6a291557f92828a4df5429dff2cb93cba4c503
86185 <86185@DESKTOP-VRRQOVO>
2020-06-17 17:31:01 +0800

    Good job

---
+++ src/com/mikes/ccncy/locks/c4/RWTreeMap.java
@@ -0,0 +1,52 @@
+package com.mikes.ccncy.locks.c4;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class RWTreeMap {
+
+	private final Map<String, Data> m = new TreeMap<String, Data>();
+	private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+	private final Lock r = rwl.readLock();
+	private final Lock w = rwl.writeLock();
+
+	public Data get(String key) {
+		r.lock();
+
+		try {
+			return m.get(key);
+		} finally {
+			r.unlock();
+		}
+	}
+
+	public String[] allKeys() {
+		r.lock();
+		try {
+			return (String[]) m.keySet().toArray();
+		} finally {
+			r.unlock();
+		}
+	}
+
+	public Data put(String key, Data value) {
+		w.lock();
+		try {
+			return m.put(key, value);
+		} finally {
+			w.unlock();
+		}
+	}
+
+	public void clear() {
+		w.lock();
+		try {
+
+		} finally {
+			w.unlock();
+		}
+	}
+
+}
