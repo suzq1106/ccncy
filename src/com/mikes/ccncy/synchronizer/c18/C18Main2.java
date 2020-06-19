de3bdb4f6733567afa14cc649c0837d52a89bc1b
suzq.it <suzq.it@suzhongqing.sanbao-steel.com>
2020-06-19 17:24:56 +0800

    Good job

--- src/com/mikes/ccncy/synchronizer/c18/C18Main2.java
+++ src/com/mikes/ccncy/synchronizer/c18/C18Main2.java
@@ -6,16 +6,16 @@ public class C18Main2 {
 
 	private static final int N = 10;
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 
-		CountDownLatch compsignal = new CountDownLatch(1);
+		CountDownLatch compsignal = new CountDownLatch(N);
 
 		for (int i = 0; i < N; i++) {
-			new Thread(new Worker2("Thread " + i, compsignal)).start();;
+			new Thread(new Worker2("Thread " + (i + 1), compsignal)).start();
 		}
-		
+
 		compsignal.await();
-		
+
 		System.out.println("fighting...");
 	}
 }
